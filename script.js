8f234557a8ece2b24c185e1aea4ec0c9a42f7284
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-05-07 13:59:00 +0400

    feat: implement sorting functionality

--- script.js
+++ script.js
@@ -69,10 +69,13 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // =============== Display movements ===============
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  // Adapt displayMovements function to support sorting
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -234,4 +237,17 @@ btnClose.addEventListener('click', function (e) {
 
   inputCloseUsername.value = '';
   inputClosePin.value = '';
+});
+
+// =============== Sorting movements ===============
+// I need sorted variable to track movements are sorted or no
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // By default sort parameter is false in displayMovements function, also sorted variable is false. When we click sort button: sort parameter becomes opposite then sorted variable.
+  displayMovements(currentAccount.movements, !sorted);
+
+  sorted = !sorted;
 });
